0b8b5ebd388a150827e88ca40582888c5e39010f
04sanjana <sanjanasn04@gmail.com>
2024-06-29 14:10:10 +0530

    updated entry app

--- app/src/main/java/com/example/entrylog/LogEntry.java
+++ app/src/main/java/com/example/entrylog/LogEntry.java
@@ -1,10 +1,21 @@
 package com.example.entrylog;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,38 +25,82 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LogEntry extends AppCompatActivity {
-    EditText ed1,ed2,ed3,ed4;
+    EditText e1,e2,e3,e4;
     AppCompatButton b1,b2;
+    String apiUrl= "http://10.0.4.16:3000/api/students";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_entry);
-        ed1=(EditText) findViewById(R.id.name);
-        ed2=(EditText) findViewById(R.id.admsn);
-        ed3=(EditText) findViewById(R.id.sys);
-        ed4=(EditText) findViewById(R.id.dept);
-        b1=(AppCompatButton) findViewById(R.id.logoutbtn);
-        b2=(AppCompatButton) findViewById(R.id.add);
-
+        e1=(EditText) findViewById(R.id.name);
+        e2=(EditText) findViewById(R.id.admsn);
+        e3=(EditText) findViewById(R.id.sys);
+        e4=(EditText) findViewById(R.id.dept);
+        b1=(AppCompatButton) findViewById(R.id.addbtn);
+        b2=(AppCompatButton) findViewById(R.id.logoutbtn);
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String getName=ed1.getText().toString();
-                String getadmn=ed2.getText().toString();
-                String getsyst=ed3.getText().toString();
-                String getdept=ed4.getText().toString();
-
-                Toast.makeText(getApplicationContext(),getName+getadmn+getsyst+getdept,Toast.LENGTH_LONG).show();
+                SharedPreferences preference = getSharedPreferences("logapp", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preference.edit();
+                editor.clear();
+                editor.apply();
+                Intent i2=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i2);
             }
+
+
+
+
+
+
         });
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+            public void onClick(View V) {
+
+                    //value reading
+                String getName=e1.getText().toString();
+                String getAdm=e2.getText().toString();
+                String getSys=e3.getText().toString();
+                String getDept=e4.getText().toString();
+
+                //json obj creation
+                JSONObject student=new JSONObject();
+                try {
+                    student.put("name",getName);
+                    student.put("admission_number",getAdm);
+                    student.put("system_number",getSys);
+                    student.put("department",getDept);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                //json obj request creation
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST,
+                        apiUrl,
+                        student,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "successfully added", Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(),"something went wrong",Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                );
+                //request queue
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jsonObjectRequest);
+
+
             }
         });
-
     }
 }
--- app/src/main/java/com/example/entrylog/MainActivity.java
+++ app/src/main/java/com/example/entrylog/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.entrylog;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -21,6 +22,12 @@ import androidx.core.view.WindowInsetsCompat;
             super.onCreate(savedInstanceState);
             EdgeToEdge.enable(this);
             setContentView(R.layout.activity_main);
+            SharedPreferences preference = getSharedPreferences ("Logapp",MODE_PRIVATE);
+            String username=preference.getString("user",null);
+            if(username != null)
+            {
+                Intent
+
 
             ed1=(EditText)findViewById(R.id.usrnme);
             ed2=(EditText)findViewById(R.id.pass);
@@ -29,18 +36,24 @@ import androidx.core.view.WindowInsetsCompat;
             b1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    String getUsername=ed1.getText().toString();
-                    String getPass=ed2.getText().toString();
-                    if(getUsername.equals("admin") & getPass.equals("12345"))
-                    {
-                        Intent i=new Intent(getApplicationContext(),LogEntry.class);
-                        startActivity(i);
-                    }
-                    else
-                    {
-                        Toast.makeText(getApplicationContext(),"Invalid ",Toast.LENGTH_LONG).show();
-                    }
-
+                 try {
+                     String getUsername = ed1.getText().toString();
+                     String getPass = ed2.getText().toString();
+                     if (getUsername.equals("admin") && getPass.equals("12345")) {
+                         SharedPreferences preference = getSharedPreferences("Logapp", MODE_PRIVATE);
+                         SharedPreferences.Editor editor = preference.edit();
+                         editor.putString("user", "admin");
+                         editor.apply();
+                         Intent i1 = new Intent(getApplicationContext(), LogEntry.class);
+                         startActivity(i1);
+                     } else {
+                         Toast.makeText(getApplicationContext(), "Invalid ", Toast.LENGTH_LONG).show();
+                     }
+                 }
+                 catch(Exception e)
+                 {
+                     Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+                 }
                 }
             });
 
